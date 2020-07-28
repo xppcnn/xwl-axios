f22f1bad91ed5b656bbfe4774876c8ae617e42c5
xuweiliang <xuweiliang@promote.cache-dns.local>
2020-07-28 20:45:40 +0800

    feat(src): 初步提交
    初步提交

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'put'
+  | 'PUT'
+  | 'post'
+  | 'POST'
+  | 'patch'
+  | 'PATCH'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
