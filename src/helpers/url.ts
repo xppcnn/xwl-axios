b41c840c8bdd1cdae4a0ad759464750134629e9c
xuweiliang <xuweiliang@promote.cache-dns.local>
2020-07-29 20:51:36 +0800

    fix(url): url处理补传
    补传

---
+++ src/helpers/url.ts
@@ -0,0 +1,51 @@
+import { isDate, isObject } from './utils'
+
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
+    return url
+  }
+  const parts: string[] = []
+  Object.entries(params).forEach(([key, value]) => {
+    // value === null  || value === undefined
+    if (value == null) {
+      return
+    }
+    let values = []
+    if (Array.isArray(value)) {
+      values = value
+      key = key + '[]'
+    } else {
+      values = [value]
+    }
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+  let serializedParams = parts.join('&')
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    // 去除 哈希
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+    //  判断url中是否含有 ？
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+  return url
+}
---
+++ src/helpers/utils.ts
@@ -0,0 +1,8 @@
+// 是否为日期类型
+export function isDate(val: any): val is Date {
+  return Object.prototype.toString.call(val) === '[object Date]'
+}
+
+export function isObject(val: any): val is Object {
+  return val !== null && typeof val === 'object'
+}
