9f1ec020b470bd56934b4244f2df1abe4a67db63
xppcnn <xppcnn@gmail.com>
2020-08-03 21:16:39 +0800

    feat(response): response处理
    对返回的response的headers以及data进行处理

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -71,6 +71,8 @@ axios({
     a: '1',
     b: '2'
   }
+}).then(res => {
+  console.log(res);
 })
 
 const arr = new Int32Array([1, 2, 3])
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,22 +1,45 @@
-import { AxiosRequestConfig } from './types'
-import { head } from 'shelljs'
+import { AxiosRequestConfig, AxiosResponse, AxiosPromise } from './types'
+import { parseHeaders } from './helpers/headers'
 
-function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'get', headers } = config
+function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise(resolve => {
+    const { data = null, url, method = 'get', headers, responseType } = config
 
-  const request = new XMLHttpRequest()
+    const request = new XMLHttpRequest()
 
-  request.open(method.toUpperCase(), url, true)
-
-  Object.keys(headers).forEach(key => {
-    // 如果没有请求体，content-type没有意义
-    if (data === null && key.toLowerCase() === 'content-type') {
-      delete headers[key]
-    } else {
-      request.setRequestHeader(key, headers[key])
+    if (responseType) {
+      request.responseType = responseType
     }
+
+    request.open(method.toUpperCase(), url, true)
+
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      resolve(response)
+    }
+
+    Object.keys(headers).forEach(key => {
+      // 如果没有请求体，content-type没有意义
+      if (data === null && key.toLowerCase() === 'content-type') {
+        delete headers[key]
+      } else {
+        request.setRequestHeader(key, headers[key])
+      }
+    })
+    request.send(data)
   })
-  request.send(data)
 }
 
 export default xhr
